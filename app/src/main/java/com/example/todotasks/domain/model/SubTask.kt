c685e7aa15f0dc21deb193cc3f48ef779c56763d
Jhon <jhonproyectos1290@gmail.com>
2026-08-19 23:09:39 +0100

    añadido de la condiciones de máxima longitud del nombre y cambio de nombre de la variable 'title' a 'tittle'

--- app/src/main/java/com/example/todotasks/domain/model/SubTask.kt
+++ app/src/main/java/com/example/todotasks/domain/model/SubTask.kt
@@ -3,6 +3,10 @@ package com.example.todotasks.domain.model
 data class SubTask(
     val id: Long = 0,
     val idTask: Long,
-    val title: String,
+    val tittle: String,
     val completed: Boolean = false,
-    val priority: TaskPriority = TaskPriority.NORMAL)
+    val priority: TaskPriority = TaskPriority.NORMAL){
+    companion object{
+        const val SUBTASK_TITTLE_MAX_LENGTH = 120
+    }
+}
