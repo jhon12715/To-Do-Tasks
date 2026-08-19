1c18ca13007ddd86e47d2a4d979542f510180674
Jhon <jhonproyectos1290@gmail.com>
2026-08-19 23:13:33 +0100

    cambio del nombre de la variable de la clase Success de 'succesMessage' a 'successMessage'

--- app/src/main/java/com/example/todotasks/ui/core/ResultEvent.kt
+++ app/src/main/java/com/example/todotasks/ui/core/ResultEvent.kt
@@ -1,6 +1,6 @@
 package com.example.todotasks.ui.core
 
 sealed class ResultEvent(val message: String) {
-    data class Success(val succesMessage: String) : ResultEvent(succesMessage)
+    data class Success(val successMessage: String) : ResultEvent(successMessage)
     data class Error(val errorMessage: String) : ResultEvent(errorMessage)
 }
