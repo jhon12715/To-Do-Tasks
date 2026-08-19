64a91ec14ea5b2403985ddf8575e3133d2644119
Jhon <jhonproyectos1290@gmail.com>
2026-08-19 23:06:00 +0100

    fichero renombrado de 'DialogDeleteTask' a 'DialogDeleteTaskCommit'

--- app/src/main/java/com/example/todotasks/ui/task/dialog/DialogDeleteTaskCommit.kt
+++ app/src/main/java/com/example/todotasks/ui/task/dialog/DialogDeleteTaskCommit.kt
@@ -3,22 +3,17 @@ package com.example.todotasks.ui.task.dialog
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
-import android.view.ViewGroup
-import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
-import com.example.todotasks.R
 import com.example.todotasks.databinding.DialogDeleteTaskBinding
-import com.example.todotasks.domain.model.Task
-import com.example.todotasks.ui.core.extensions.applyWindowApparence
-import com.example.todotasks.ui.core.extensions.backgroundMoreWhite
+import com.example.todotasks.ui.core.dialog_base_commit_action.DialogBaseCommitAction
 import com.example.todotasks.ui.task.task_list.TaskListViewModel
+import com.example.todotasks.ui.task.task_list.TaskUiEvent
 
-class DialogDeleteTask(
-    private val task: Task
-) : DialogFragment() {
+class DialogDeleteTaskCommit() : DialogBaseCommitAction() {
 
-    private lateinit var binding: DialogDeleteTaskBinding
     private val viewModel: TaskListViewModel by activityViewModels()
+
+
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         binding = DialogDeleteTaskBinding.inflate(layoutInflater)
@@ -31,22 +26,10 @@ class DialogDeleteTask(
         return builder.create()
     }
 
-    override fun onStart() {
-        super.onStart()
-        binding.clParent.backgroundMoreWhite(0.9f)
-        applyWindowApparence()
-    }
-
-    private fun startUI() {
-        binding.tvTittle.text = "Eliminar Tarea"
-        binding.tvMessage.text = "seguro que desea borrar la tarea: ${task.task}"
-
-    }
-
-    private fun setListeners() {
+    override fun setListeners() {
         binding.btnCancel.setOnClickListener { dismiss() }
         binding.btnAccept.setOnClickListener {
-            //viewModel.deleteTask(task)
+            viewModel.onEvent(TaskUiEvent.CommitTaskToDelete)
             dismiss()
         }
     }
