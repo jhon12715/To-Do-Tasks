8b5f8dd91fd5e766ecc340a146b437f26092c115
Jhon <jhonproyectos1290@gmail.com>
2026-08-19 21:58:00 +0100

    Separado de la clase de filtrado por tarea o subtarea completada

---
+++ app/src/main/java/com/example/todotasks/domain/model/IsCompletedFilter.kt
@@ -0,0 +1,12 @@
+package com.example.todotasks.domain.model
+
+/**
+ * Project: To Do Tasks
+ * Created by: Jhon
+ */
+
+enum class IsCompletedFilter{
+    ALL,
+    COMPLETED,
+    NOT_COMPLETED
+}
