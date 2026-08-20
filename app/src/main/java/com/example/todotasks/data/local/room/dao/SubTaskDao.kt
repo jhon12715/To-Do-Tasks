abc30b2843648dee197fd7387f32d433816e7f84
Jhon <jhonproyectos1290@gmail.com>
2026-08-20 18:54:39 +0100

    cambio en la función de obtener todas las subtasks del objeto 'SubTask' por 'SubtaskEntity' y de la función de borrado solo individual a borrado de una a varias y añadido de la función de obtener una subtarea por su id

--- app/src/main/java/com/example/todotasks/data/local/room/dao/SubTaskDao.kt
+++ app/src/main/java/com/example/todotasks/data/local/room/dao/SubTaskDao.kt
@@ -11,18 +11,21 @@ import kotlinx.coroutines.flow.Flow
 interface SubTaskDao {
 
     @Query("SELECT * FROM subTask WHERE idTask = :idTask")
-    fun getAllSubTasks(idTask: Long): Flow<List<SubTask>>
+    fun getAllSubTasks(idTask: Long): Flow<List<SubTaskEntity>>
 
     @Upsert
     suspend fun upsertSubTask(subTask: SubTaskEntity)
 
-    @Query("DELETE FROM subTask WHERE id = :id")
-    suspend fun deleteSubTask(id: Long)
+    @Query("DELETE FROM subTask WHERE id IN (:listSubTaskId)")
+    suspend fun deleteSubTask(listSubTaskId: List<Long>)
 
     @Query("UPDATE subTask SET completed = :completed WHERE id = :id")
     suspend fun updateCompletedSubTask(id: Long, completed: Boolean)
 
     @Query("SELECT EXISTS (SELECT * FROM subTask WHERE title = :name AND id != :subTaskId AND idTask = :taskId COLLATE NOCASE)")
     suspend fun subTaskExists(name: String, subTaskId: Long, taskId: Long): Boolean
+
+    @Query("SELECT * FROM subTask WHERE id = :subTaskId")
+    suspend fun getSubtaskById(subTaskId: Long): SubTaskEntity
 
 }
