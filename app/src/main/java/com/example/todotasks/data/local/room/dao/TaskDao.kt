b92c02b98b5ac6c5a02db69d2a0661a514d89490
Jhon <jhonproyectos1290@gmail.com>
2026-08-20 18:52:42 +0100

    cambio en el nombre del atributo 'task' por 'tittle' y cambio de la función de borrado solo individual a borrado de una a varias

--- app/src/main/java/com/example/todotasks/data/local/room/dao/TaskDao.kt
+++ app/src/main/java/com/example/todotasks/data/local/room/dao/TaskDao.kt
@@ -12,28 +12,28 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface TaskDao {
 
-    @Query("SELECT t.id, t.task, (SELECT COUNT(*) FROM subtask s WHERE s.idTask = t.id AND s.completed = 1) as completedSubtask, (SELECT COUNT(*) FROM subtask s WHERE s.idTask = t.id) as totalSubTask, isCompleted, priority, date, category FROM task t WHERE category = :categoryId")
+    @Query("SELECT t.id, t.tittle, (SELECT COUNT(*) FROM subtask s WHERE s.idTask = t.id AND s.completed = 1) as completedSubtask, (SELECT COUNT(*) FROM subtask s WHERE s.idTask = t.id) as totalSubTask, isCompleted, priority, date, categoryId FROM task t WHERE categoryId = :categoryId")
     fun getAllTasksByCategory(categoryId: Long): Flow<List<TaskWithStatsProjection>>
 
-    @Query("SELECT t.id, t.task, (SELECT COUNT(*) FROM subtask s WHERE s.idTask = t.id AND s.completed = 1) as completedSubtask, (SELECT COUNT(*) FROM subtask s WHERE s.idTask = t.id) as totalSubTask, isCompleted, priority, date, category FROM task t")
+    @Query("SELECT t.id, t.tittle, (SELECT COUNT(*) FROM subtask s WHERE s.idTask = t.id AND s.completed = 1) as completedSubtask, (SELECT COUNT(*) FROM subtask s WHERE s.idTask = t.id) as totalSubTask, isCompleted, priority, date, categoryId FROM task t")
     fun getAllTasks(): Flow<List<TaskWithStatsProjection>>
 
     @Query("SELECT * FROM task WHERE id = :id")
     suspend fun getTask(id: Long): Task
 
-    @Query("SELECT id as taskId, task as taskName FROM task WHERE id = :id")
+    @Query("SELECT id as taskId, tittle as taskName FROM task WHERE id = :id")
     suspend fun getParentTaskInfo(id: Long): ParentTaskInfo
 
     @Upsert
     suspend fun upsertTask(task: TaskEntity): Long
 
-    @Query("DELETE FROM task WHERE id = :id")
-    suspend fun deleteTask(id: Long)
+    @Query("DELETE FROM task WHERE id IN(:listId)")
+    suspend fun deleteTasks(listId: List<Long>)
 
     @Query("UPDATE task SET isCompleted = :isCompleted WHERE id = :id")
     suspend fun updateCompletedTask(id: Long, isCompleted: Boolean)
 
-    @Query("SELECT EXISTS(SELECT * FROM task WHERE task = :name AND id != :id COLLATE NOCASE)")
+    @Query("SELECT EXISTS(SELECT * FROM task WHERE tittle = :name AND id != :id COLLATE NOCASE)")
     suspend fun taskExists(name: String, id: Long): Boolean
 
 }
