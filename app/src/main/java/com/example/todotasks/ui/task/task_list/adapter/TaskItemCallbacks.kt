16e35002b279c73a187e7e13b13e5fb105b9dfba
Jhon <jhonproyectos1290@gmail.com>
2026-08-20 18:51:21 +0100

    cambio en le atributo de borrar individual por los de añadir la id o borrar la id de la lista de los que van a ser borrados

--- app/src/main/java/com/example/todotasks/ui/task/task_list/adapter/TaskItemCallbacks.kt
+++ app/src/main/java/com/example/todotasks/ui/task/task_list/adapter/TaskItemCallbacks.kt
@@ -1,13 +1,16 @@
-package com.example.todotasks.ui.task
+package com.example.todotasks.ui.task.task_list.adapter
 
 import com.example.todotasks.domain.model.Task
 import com.example.todotasks.domain.model.TaskPriority
+import com.example.todotasks.ui.model.ScreenMode
 import com.example.todotasks.ui.model.TaskUI
 import java.time.LocalDate
 
 data class TaskItemCallbacks (
     val editTask: (TaskUI) -> Unit,
     val openSubTaskActivity: (Long, String) -> Unit,
-    val deleteTask: (Task) -> Unit,
-    val updateCompletedTask: (Long, Boolean) -> Unit
+    val updateCompletedTask: (Long, Boolean) -> Unit,
+    val updateScreenMode: (ScreenMode) -> Unit,
+    val addTaskToDelete: (Long) -> Unit,
+    val removeTaskToDelete: (Long) -> Unit
 )
