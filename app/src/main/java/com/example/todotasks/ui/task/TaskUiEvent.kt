55731d1eff7854b57a9a870816273bc6106edfac
Jhon <jhonproyectos1290@gmail.com>
2026-09-11 17:08:49 +0100

    añadido de los eventos de actualizar, añadir, borrar y hacer commit en las tareas para borrar y fichero movido fuera de la carpeta 'task_list'

--- app/src/main/java/com/example/todotasks/ui/task/TaskUiEvent.kt
+++ app/src/main/java/com/example/todotasks/ui/task/TaskUiEvent.kt
@@ -1,9 +1,10 @@
-package com.example.todotasks.ui.task.task_list
+package com.example.todotasks.ui.task
 
 import com.example.todotasks.domain.model.GroupByTask
 import com.example.todotasks.domain.model.SortByTask
-import com.example.todotasks.domain.model.TaskIsCompletedFilter
+import com.example.todotasks.domain.model.IsCompletedFilter
 import com.example.todotasks.ui.model.CategoryUI
+import com.example.todotasks.ui.model.ScreenMode
 
 /**
  * Project: To Do Tasks
@@ -11,11 +12,14 @@ import com.example.todotasks.ui.model.CategoryUI
  */
 
 sealed class TaskUiEvent {
-    data class UpdateTaskFilter(val taskIsCompletedFilter: TaskIsCompletedFilter) : TaskUiEvent()
+    data class UpdateTaskIsCompletedFilter(val taskIsCompletedFilter: IsCompletedFilter) : TaskUiEvent()
     data class UpdateCategoryTaskSelected(val categorySelectedId: Long) : TaskUiEvent()
     data class UpdateCompletedTask(val taskId: Long, val isCompleted: Boolean) : TaskUiEvent()
-    data class DeleteTask(val taskId: Long) : TaskUiEvent()
     data class DeleteCategory(val  category: CategoryUI): TaskUiEvent()
     data class UpdateGroupBy(val  groupBy: GroupByTask): TaskUiEvent()
     data class UpdateSortBy(val  sortBy: SortByTask): TaskUiEvent()
+    data class UpdateScreenMode(val screenMode: ScreenMode): TaskUiEvent()
+    data class AddTaskToDelete(val taskId: Long): TaskUiEvent()
+    data class RemoveTaskToDelete(val taskId: Long): TaskUiEvent()
+    data object CommitTaskToDelete: TaskUiEvent()
 }
